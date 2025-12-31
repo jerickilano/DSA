cd4d701a46c6d7a6865edbae175d88a6dca64475
jerickilano <122631469+jerickilano@users.noreply.github.com>
2025-12-30 20:39:43 -0800

    Time: 419 ms (94.20%) | Memory: 20.3 MB (79.25%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,32 +1,33 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # Time: O(n^2) Space: O(m) m the number of triplets
-        # Two Pointers w/ .sort()
-        # [a,b,c,d,e,f,g]
-        #  i,L, , , , ,R
-
+        # 3 pointer + list
+        # [-1,0,1,2,-1,-4]
+        #   i L         R
         res = []
         nums.sort()
 
-        for i, a in enumerate(nums): # iterate through the index and the value
-            if a > 0:
+        for i, val in enumerate(nums):
+            if val > 0:
                 break
 
-            if i > 0 and a == nums[i - 1]: # avoid duplicate values
+            if i > 0 and val == nums[i - 1]:
                 continue
 
             l, r = i + 1, len(nums) - 1
             while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
-                    l += 1
-                else:
-                    res.append([a, nums[l], nums[r]])
+                three_sum = val + nums[l] + nums[r]
+                if three_sum == 0:
+                    res.append([val, nums[l],nums[r]])
                     l += 1
                     r -= 1
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
-
+                elif three_sum > 0:
+                    r -= 1
+                else:
+                    l += 1
+        
         return res
+                
+        
+            

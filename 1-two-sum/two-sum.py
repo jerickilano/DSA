ef1d426c5354546dc08e633b64002939b316703f
jerickilano <122631469+jerickilano@users.noreply.github.com>
2026-01-02 21:58:59 -0800

    Time: 3 ms (50.42%) | Memory: 18.7 MB (57.10%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,13 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Time: O(n) Space: O(n)
-        # HashMap (one pass)/for each num in enum(nums)/check 
-        # if diff is in map/return index of diff AND current i
-        
-        map = {}
-        for i, num in enumerate(nums):
-            diff = target - nums[i]
-            if diff in map:
-                return [map[diff], i]
-            map[num] = i
+        # Time: O(n) Space: O(N)
+        seen = {}
+        for i, num in enumerate(nums): # Enumerate to have access to the indices of the array
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
         return [0,0]
